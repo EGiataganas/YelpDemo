8fa086635b391b961845e4df14137acf0cb44c26
Evangelos Giataganas <e.giataganas@gmail.com>
2015-07-16 13:15:37 +0100

    add bootstrap classes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,8 @@ Rails.application.routes.draw do
 
   root 'restaurants#index'
   
-  get 'pages/about'
-
-  get 'pages/contact'
+  get 'about' => 'pages#about'
+  get 'contact' => 'pages#contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
