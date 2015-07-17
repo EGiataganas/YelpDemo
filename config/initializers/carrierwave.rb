4d635fc043c1967e1bd299e6987eb49ab4a2946d
Evangelos Giataganas <e.giataganas@gmail.com>
2015-07-17 11:15:18 +0100

    remove fog_provider

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,5 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
-    config.fog_provider = 'fog/aws'                        # required
     config.fog_credentials = {
       provider:              'AWS',                        # required
       aws_access_key_id:     ENV['aws_access_key_id'],                        # required
