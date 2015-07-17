66369faf234095057144dc2e04cc849285e8d412
Evangelos Giataganas <e.giataganas@gmail.com>
2015-07-17 14:02:10 +0100

    hopefully it will be the last conf file...

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,6 +7,22 @@ if Rails.env.production?
       endpoint:              ENV['aws_endpoint'],
       region:               ENV['aws_region'] 
     }
+ 
+    # For testing, upload files to local `tmp` folder.
+    if Rails.env.test? || Rails.env.cucumber?
+      config.storage = :file
+      config.enable_processing = false
+      config.root = "#{Rails.root}/tmp"
+    else
+      config.storage = :fog
+    end
+   
+    config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
+   
+    config.fog_directory    = ENV['fog_directory']
+    config.s3_access_policy = :public_read                          # Generate http:// urls. Defaults to :authenticated_read (https://)
+    config.fog_host         = "#{ENV['aws_endpoint']}/#{ENV['fog_directory']}"
+
     config.fog_directory  = ENV['fog_directory']                          # required
   end
 end
