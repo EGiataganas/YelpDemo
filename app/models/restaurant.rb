53add5ea9804bd56f12a8d9c5e62848b979ce6dd
Evangelos Giataganas <e.giataganas@gmail.com>
2015-07-26 15:40:46 +0100

    fix regex for address

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,7 +5,7 @@ class Restaurant < ActiveRecord::Base
 
   validates :name, :address, :phone, :website, :image, presence: true
   
-  validates :address, format: { with: /\\A\d+[^,]+,[^,]+, [A-Z]{2} \d{5}\z/,
+  validates :address, format: { with: /\A\d+[^,]+,[^,]+, [A-Z]{2} \d{5}\z/,
     message: "must be in the format 350 Fith Avenue, New York, NY 10118" }
   validates :phone, format: { with: /\A\(\d{3}\) \d{3}\-\d{4}\z/,
     message: "must be in the format (123) 456-7890" }
